e432f1d819bdacb980864275d998b7fdcf4bf05d
Hamid Mallhi <hamidmallhi@Hamids-MacBook-Pro.local>
2021-06-08 20:45:41 -0600

    day 4 ammended permissions file added to .env

--- src/components/Header.js
+++ src/components/Header.js
@@ -74,6 +74,7 @@ function Header() {
       </div>
 
       {/* Bottom */}
+      {/* Bottom */}
       <div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm">
         <p className="link flex items-center">
           <MenuIcon className="h-6 mr-1" /> All
