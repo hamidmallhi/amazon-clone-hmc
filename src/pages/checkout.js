1ee107554b3377e20d3c7483871375d55bea1e9b
Hamid Mallhi <hamidmallhi@Hamids-MacBook-Pro.local>
2021-06-14 11:30:41 -0600

    google functions added for env 3

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,207 +1,99 @@
-// import React from 'react'
-// import Header from "../components/Header";
-// import Image from "next/image";
-// import { useSelector } from "react-redux";
-// import { selectItems, selectTotal } from "../slices/basketSlice";
-// import CheckoutProduct from "../components/CheckoutProduct";
-// import NumberFormat from 'react-number-format';
-// import axios from 'axios';
-// import { useSession } from "next-auth/client";
-// import { loadStripe } from "@stripe/stripe-js"
-// const stripePromise = loadStripe(process.env.stripe_public_key)
-
-// function Checkout() {
-//   const items = useSelector(selectItems);
-//   const total = useSelector(selectTotal);
-
-//   const [session] = useSession();
-
-//   const createCheckoutSession = async () => {
-//       const stripe= await stripePromise
-
-//       // call the backend to create a checkout session...
-//       const checkoutSession = await axios.post('/api/create-checkout-session', 
-//       {
-//         items: items,
-//         email: session.user.email,
-//       })
-//       // redirect user/customer to stripe checkout
-//       const result = await stripe.redirectToCheckout({
-//         sessionId: checkoutSession.data.id
-//       })
-//       if(result.error) alert(result.error.message)
-//   }
-
-//   return (
-//     <div className="bg-gray-100">
-//       <Header />
-
-//       <main className="lg:flex max-w-screen-2xl mx-auto">
-//         {/* left */}
-//         <div className="flex-grow m-5 shadow-sm">
-//           <Image 
-//           src="https://links.papareact.com/ikj"
-//           width={1020}
-//           height={250}
-//           objectFit="contain"
-//           />
-//           <div className="flex flex-col p-5 space-y-10 bg-white">
-//             <h1 className="text-3xl border-b pb-4">
-//               {items.length === 0 ? 'Your Amazon Basket is Empty' : 'Shopping Basket'}
-//             </h1>
-//             {items.map((item, i) => (
-//               <CheckoutProduct 
-//               key={i}
-//               id={item.id}
-//               title={item.title}
-//               rating={item.rating}
-//               price={item.price}
-//               description={item.description}
-//               category={item.category}
-//               image={item.image}
-//               hasPrime={item.hasPrime}
-//               />
-//             ))}
-//           </div>
-//         </div>
-
-//         {/* right */}
-//         <div className="flex flex-col bg-white p-10 shadow-md">
-//           {items.length > 0 && (
-//             <>
-//               <h2 className="whitespace-nowrap">
-//                 Subtotal ({items.length} items):
-//                 <span className="font-bold ml-2">
-//                 <NumberFormat value={total} displayType={'text'} thousandSeparator={true} prefix={'$'} />
-//                 </span>
-//               </h2>
-//               <button
-//               role="link" 
-//               onClick={createCheckoutSession}
-//               disabled={!session}
-//               className={`button mt-2 ${!session && 'from-gray-300 to-gray-500 border-gray-100 text-gray-300 cursor-not-allowed'}`}>
-//                   {!session ? 'Sign In to checkout' : 'Proceed to checkout' }
-//               </button>
-//             </>
-//           )}
-//         </div>
-//       </main>
-//     </div>
-//   )
-// }
-
-// export default Checkout
-
-
-
-
-
-
+import React from 'react'
 import Header from "../components/Header";
 import Image from "next/image";
-import { selectItems, selectTotal } from "../slices/basketSlice";
 import { useSelector } from "react-redux";
-import NumberFormat from 'react-number-format';
+import { selectItems, selectTotal } from "../slices/basketSlice";
 import CheckoutProduct from "../components/CheckoutProduct";
-import { loadStripe } from "@stripe/stripe-js";
-import axios from "axios";
+import NumberFormat from 'react-number-format';
+import axios from 'axios';
 import { useSession } from "next-auth/client";
-const stripePromise = loadStripe(process.env.stripe_public_key);
+import { loadStripe } from "@stripe/stripe-js"
+const stripePromise = loadStripe(process.env.stripe_public_key)
 
 function Checkout() {
   const items = useSelector(selectItems);
   const total = useSelector(selectTotal);
+
   const [session] = useSession();
 
   const createCheckoutSession = async () => {
-    // Get Stripe.js instance
-    const stripe = await stripePromise;
+      const stripe= await stripePromise
 
-    // Call your backend to create the Checkout Session
-    const checkoutSession = await await axios.post(
-      "/api/create-checkout-session",
+      // call the backend to create a checkout session...
+      const checkoutSession = await axios.post('/api/create-checkout-session', 
       {
-        items,
+        items: items,
         email: session.user.email,
-      }
-    );
-
-    // Redirect Customer to Checkout
-    const result = await stripe.redirectToCheckout({
-      sessionId: checkoutSession.data.id,
-    });
-
-    if (result.error) {
-      alert(result.error.message);
-    }
-  };
+      })
+      // redirect user/customer to stripe checkout
+      const result = await stripe.redirectToCheckout({
+        sessionId: checkoutSession.data.id
+      })
+      if(result.error) alert(result.error.message)
+  }
 
   return (
     <div className="bg-gray-100">
       <Header />
 
       <main className="lg:flex max-w-screen-2xl mx-auto">
-        {/* Left */}
+        {/* left */}
         <div className="flex-grow m-5 shadow-sm">
-          <Image
-            src="https://links.papareact.com/ikj"
-            width={1020}
-            height={250}
-            objectFit="contain"
+          <Image 
+          src="https://links.papareact.com/ikj"
+          width={1020}
+          height={250}
+          objectFit="contain"
           />
-
           <div className="flex flex-col p-5 space-y-10 bg-white">
             <h1 className="text-3xl border-b pb-4">
-              {items.length === 0
-                ? "Your Amazon Basket is empty."
-                : "Shopping Basket"}
+              {items.length === 0 ? 'Your Amazon Basket is Empty' : 'Shopping Basket'}
             </h1>
-
             {items.map((item, i) => (
-              <CheckoutProduct
-                key={i}
-                id={item.id}
-                title={item.title}
-                rating={item.rating}
-                price={item.price}
-                description={item.description}
-                category={item.category}
-                image={item.image}
-                hasPrime={item.hasPrime}
+              <CheckoutProduct 
+              key={i}
+              id={item.id}
+              title={item.title}
+              rating={item.rating}
+              price={item.price}
+              description={item.description}
+              category={item.category}
+              image={item.image}
+              hasPrime={item.hasPrime}
               />
             ))}
           </div>
         </div>
 
-        {/* Right */}
-        <div className="flex flex-col bg-white shadow-sm p-10">
+        {/* right */}
+        <div className="flex flex-col bg-white p-10 shadow-md">
           {items.length > 0 && (
             <>
               <h2 className="whitespace-nowrap">
-                Subtotal ({items.length} items):{" "}
-                <span className="font-bold">
-                 
-                  <NumberFormat value={total} displayType={'text'} thousandSeparator={true} prefix={'$'} />
+                Subtotal ({items.length} items):
+                <span className="font-bold ml-2">
+                <NumberFormat value={total} displayType={'text'} thousandSeparator={true} prefix={'$'} />
                 </span>
               </h2>
               <button
-                disabled={!session}
-                onClick={createCheckoutSession}
-                role="link"
-              
-                className={`button mt-2 ${
-                  !session &&
-                  "from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed"
-                }`}
-              >
-                {!session ? "Sign In to Checkout" : "Proceed to Checkout"}
+              role="link" 
+              onClick={createCheckoutSession}
+              disabled={!session}
+              className={`button mt-2 ${!session && 'from-gray-300 to-gray-500 border-gray-100 text-gray-300 cursor-not-allowed'}`}>
+                  {!session ? 'Sign In to checkout' : 'Proceed to checkout' }
               </button>
             </>
           )}
         </div>
       </main>
     </div>
-  );
+  )
 }
 
-export default Checkout;
+export default Checkout
+
+
+
+
+
+
