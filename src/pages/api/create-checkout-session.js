bdf0c8468de44083ec8c3ba5ad897735ee11967f
Hamid Mallhi <hamidmallhi@Hamids-MacBook-Pro.local>
2021-06-14 11:03:01 -0600

    google functions added for env 3

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -1,35 +1,82 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
+
+// export default async (req, res) => {
+//   const { items, email } = req.body
+//   const transformedItems = items.map(item =>({
+//     description: item.description,
+//     quantity: 1,
+//     price_data: {
+//       currency: 'usd',
+//       unit_amount: item.price * 100,
+//       product_data: {
+//         name: item.title,
+//         images: [item.image]
+//       },
+//     }
+//   }))
+// const session = await stripe.checkout.sessions.create({
+//   payment_method_types: ["card"],
+//   shipping_rates: ["shr_1IyJvtHJ56WtV3PQxGMSgC4a"],
+//   shipping_address_collection: { allowed_countries: ['GB', 'US', 'CA'] },
+//   line_items: 
+//     transformedItems,
+//     mode: "payment",
+//     success_url: `${process.env.HOST}/success`,
+//     cancel_url: `${process.env.HOST}/checkout`,
+//     metadata: {
+//       email,
+//       images: JSON.stringify(items.map(item => item.image))
+//     }
+// })
+//   res.status(200).json({id: session.id})
+// }
+
+
+
+
+
+
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export default async (req, res) => {
-  const { items, email } = req.body
-  const transformedItems = items.map(item =>({
+  const { items, email } = req.body;
+
+  const transformedItems = items.map((item) => ({
+    price_data: {
+      currency: "gbp",
+      product_data: {
+        images: [item.image],
+        name: item.title,
+      },
+      unit_amount: item.price * 100,
+    },
     description: item.description,
     quantity: 1,
-    price_data: {
-      currency: 'usd',
-      unit_amount: item.price * 100,
-      product_data: {
-        name: item.title,
-        images: [item.image]
-      },
-    }
-  }))
-const session = await stripe.checkout.sessions.create({
-  payment_method_types: ["card"],
-  shipping_rates: ["shr_1IyJvtHJ56WtV3PQxGMSgC4a"],
-  shipping_address_collection: { allowed_countries: ['GB', 'US', 'CA'] },
-  line_items: 
-    transformedItems,
+  }));
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    shipping_rates: ["shr_1IyJvtHJ56WtV3PQxGMSgC4a"],
+    shipping_address_collection: {
+      allowed_countries: ["GB", "US", "CA"],
+    },
+    line_items: transformedItems,
     mode: "payment",
     success_url: `${process.env.HOST}/success`,
     cancel_url: `${process.env.HOST}/checkout`,
     metadata: {
       email,
-      images: JSON.stringify(items.map(item => item.image))
-    }
-})
-  res.status(200).json({id: session.id})
-}
+      images: JSON.stringify(items.map((item) => item.image)),
+    },
+  });
+
+  res.status(200).json({ id: session.id });
+};
+
+// Use this to test the API and create a session ID
+// curl -X POST -is "http://localhost:3000/api/create-checkout-session" -d ""
+
 
 
